6218104b28ad081ef49c2d52ed8bd78547dd2416
Attila Gy. Molnar <attila.gymolnar@gmail.com>
2019-12-08 16:47:42 +0100

    update start_timez.sh

--- util/start_timez.sh
+++ util/start_timez.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
-# this is for desktop environment where the window manager
-# can be configured with wmctrl
-# in this example: set the 'Always on top' flag
-
-#python $HOME/lib/python2.7/timez.py &
-python /usr/local/lib/python2.7/timez.py &
+python2.7 ~/.local/lib/python2.7/timez.py &
 PID=$!
 sleep 1
 
-info=`wmctrl -p -l -G | awk "/$PID/ {print}"`
-if [ "$info" ]
-then
-    wid=`echo $info | awk '{print $1}'`
-    wmctrl -i -r $wid -b add,above
-else
-    exit 1
+if [ -e /proc/$PID ]; then
+	info=`wmctrl -p -l -G | awk "/$PID/ {print}"`
+	if [ "$info" ]; then
+		wid=`echo $info | awk '{print $1}'`
+		wmctrl -i -r $wid -b add,above
+		wmctrl -i -r $wid -b add,skip_taskbar
+		wmctrl -i -r $wid -b add,skip_pager
+		exit 0
+	fi
 fi
+exit 1
